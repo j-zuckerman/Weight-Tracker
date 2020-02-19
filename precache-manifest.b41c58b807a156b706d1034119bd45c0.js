7823853b20f0e06ece2eff7d6a770af882055ccb
James Zuckerman <jzuckerman4@gmail.com>
2020-02-19 14:20:41 -0700

    Updates

---
+++ precache-manifest.b41c58b807a156b706d1034119bd45c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1448b78934dbccaef2e46bf0cd914e",
+    "url": "/Weight-Tracker/index.html"
+  },
+  {
+    "revision": "b7aef890ca1c34a1e4d8",
+    "url": "/Weight-Tracker/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "0516cf22b94d14641cc7",
+    "url": "/Weight-Tracker/static/css/main.00a837a8.chunk.css"
+  },
+  {
+    "revision": "b7aef890ca1c34a1e4d8",
+    "url": "/Weight-Tracker/static/js/2.ebe08ad6.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/Weight-Tracker/static/js/2.ebe08ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0516cf22b94d14641cc7",
+    "url": "/Weight-Tracker/static/js/main.ae1794f4.chunk.js"
+  },
+  {
+    "revision": "ba5412b9ce6e7ec48777",
+    "url": "/Weight-Tracker/static/js/runtime-main.48a6a28b.js"
+  }
+]);
